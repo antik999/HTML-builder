15e38b51f90cfe07c7b8bc07ae1a0123b936c554
antik999 <antikill-fc@yandex.ru>
2023-05-08 18:42:21 +0300

    fix: 04-copy-directory complete

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,36 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+async function copyDir() {
+  const srcDir = path.join(__dirname, 'files');
+  const destDir = path.join(__dirname, 'files-copy');
+
+  try {
+    // Создание папки files-copy в случае если она ещё не существует
+    await fs.mkdir(destDir, { recursive: true });
+    
+    // получаем содержимое папки-источника
+    const files = await fs.readdir(srcDir, { withFileTypes: true });
+    
+    // копирование каждого файла из папки-источника в папку назначения
+    for (const file of files) {
+      const srcPath = path.join(srcDir, file.name);
+      const destPath = path.join(destDir, file.name);
+      
+      if (file.isDirectory()) {
+        // рекурсивное копирование, если есть подпапки
+        await copyDir(srcPath, destPath);
+      } else {
+        // копирование файлов
+        await fs.copyFile(srcPath, destPath);
+      }
+    }
+    
+    console.log('Папка успешно скопирована');
+  } catch (error) {
+    console.error(`Ошибка: ${error}`);
+  }
+}
+
+copyDir();
 
